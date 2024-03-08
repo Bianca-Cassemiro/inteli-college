a71253a3ac047528ae3c6561b820cba016e4047c
Bianca-Cassemiro <cassemirobiancalima@gmail.com>
2024-03-08 11:03:53 -0300

    Prova1

---
+++ prova1/sub/subscriber.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	godotenv "github.com/joho/godotenv"
+)
+
+var messagePubHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("Recebido: %s do tópico: %s\n", msg.Payload(), msg.Topic())
+}
+
+func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		fmt.Printf("Error loading .env file: %s", err)
+	}
+
+	var broker = os.Getenv("BROKER_ADDR")
+	var port = 8883
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tls://%s:%d", broker, port))
+	opts.SetClientID("Subscriber")
+	opts.SetUsername(os.Getenv("HIVE_USER"))
+	opts.SetPassword(os.Getenv("HIVE_PSWD"))
+	opts.SetDefaultPublishHandler(messagePubHandler)
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	if token := client.Subscribe("Bia", 1, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		return
+	}
+
+	fmt.Println("Subscriber está rodando. Pressione CTRL+C para sair.")
+	select {}
+}

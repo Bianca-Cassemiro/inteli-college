348c2216572d0a7103c8a303ea0110c6298c54e0
Bianca-Cassemiro <cassemirobiancalima@gmail.com>
2024-04-09 10:30:38 -0300

    Revert "integração kafka"
    This reverts commit 1bfe6a6cc81ea79d142450369bb5e8e49beab08a.

--- atividade_1/pub/publisher.go
+++ atividade_1/pub/publisher.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 	"time"
-
+	godotenv "github.com/joho/godotenv"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson"
+
 )
 
 type SensorData struct {
@@ -28,30 +24,10 @@ func main() {
 		fmt.Printf("Error loading .env file: %s", err)
 	}
 
-	// Configurações do produtor Kafka
 	producer, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:29092,localhost:39092",
 		"client.id":         "go-producer",
 	})
-	if err != nil {
-		fmt.Printf("Erro ao criar o produtor: %v\n", err)
-		return
-	}
-	defer producer.Close()
-
-	// Conexão com o MongoDB Atlas
-	mongoURI := "mongodb+srv://@cluster-bia.htq4jn2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-Bia"
-	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
 
 	// Abre arquivo JSON
 	file, err := os.Open("../sensor_data.json")
@@ -65,29 +41,37 @@ func main() {
 		panic(err)
 	}
 
-	// Loop para publicar mensagens continuamente
-	for {
-		for _, sensorData := range sensorDataList {
+	// Configuração do cliente MQTT
+	var broker = os.Getenv("BROKER_ADDR")
+	var port = 8883
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tls://%s:%d", broker, port))
+	opts.SetClientID("Publisher")
+	opts.SetUsername(os.Getenv("HIVE_USER"))
+	opts.SetPassword(os.Getenv("HIVE_PSWD"))
 	
 
-						message := fmt.Sprintf("Valor: %.2f %s\nTimestamp: %s\nLocalização: %s", sensorData.Value, sensorData.Unit, sensorData.Timestamp, sensorData.Location)
-						topic := "KafkaBia"
-						producer.Produce(&kafka.Message{
-							TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-							Value:          []byte(message),
-						}, nil)
-						fmt.Printf("Leitura do sensor:\n\n%s\n\n", message)
-
-						// Salvar a mensagem no MongoDB
-						collection := client.Database("sensor_data").Collection("data") 
-						_, err := collection.InsertOne(ctx, bson.M{"message": message})
-						if err != nil {
-							log.Println("Erro ao salvar a mensagem no MongoDB:", err)
-							continue
-						}
-			log.Println("Mensagem salva no MongoDB:", message)
-
-			time.Sleep(2 * time.Second)
-		}
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
 	}
-}
+
+	// Loop para publicar mensagens continuamente
+
+		for {
+			for _, sensorData := range sensorDataList {
+				message := fmt.Sprintf("Valor: %.2f %s\nTimestamp: %s\nLocalização: %s", sensorData.Value, sensorData.Unit, sensorData.Timestamp, sensorData.Location)
+				token := client.Publish("KafkaBia", 0, false, message)
+				topic := "KafkaBia"
+				producer.Produce(&kafka.Message{
+					TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+					Value:          []byte(message),
+				}, nil)
+				token.Wait()
+				fmt.Printf("Leitura do sensor:\n\n%s\n\n", message)
+				time.Sleep(2 * time.Second)
+			}
+		}
+			
+	}
+	
